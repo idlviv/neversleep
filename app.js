0e8d8a70edb7f03caa5e039cf44a615b13f36f01
idlviv <idlviv@gmail.com>
2019-01-28 11:32:55 +0200

    +

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
 var http = require("http");
 setInterval(function() {
-    http.get("https://hmadedotwork.herokuapp.com");
-}, 1200000); // every 20 minutes
+
+http.get('http://hmadedotwork.herokuapp.com', (res) => {
+//   const { statusCode } = res;
+//   const contentType = res.headers['content-type'];
+// console.log('statusCode', statusCode);
+
+//   let error;
+//   if (statusCode !== 200) {
+//     error = new Error('Request Failed.\n' +
+//                       `Status Code: ${statusCode}`);
+//   } else if (!/^application\/json/.test(contentType)) {
+//     error = new Error('Invalid content-type.\n' +
+//                       `Expected application/json but received ${contentType}`);
+//   }
+//   if (error) {
+//     console.error(error.message);
+//     // consume response data to free up memory
+//     res.resume();
+//     return;
+//   }
+
+//   res.setEncoding('utf8');
+//   let rawData = '';
+//   res.on('data', (chunk) => { rawData += chunk; });
+//   res.on('end', () => {
+//     try {
+//       const parsedData = JSON.parse(rawData);
+//       console.log(parsedData);
+//     } catch (e) {
+//       console.error(e.message);
+//     }
+//   });
+}).on('error', (e) => {
+  console.error(`Got error: ${e.message}`);
+});
+
+}, 900000); // every 15 minutes
